08a38801c6860d8b6236e6f2cf6002c8e54bb8f6
laqiiz <mano.junki@gmail.com>
2021-09-18 13:12:51 +0900

    Add sample code

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/ma91n/apigw-test-invoke-method-go
+
+go 1.17
+
+require github.com/aws/aws-sdk-go v1.40.45
+
+require github.com/jmespath/go-jmespath v0.4.0 // indirect
---
+++ sample.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/apigateway"
+	"log"
+	"net/http"
+)
+
+var apigw = apigateway.New(session.Must(session.NewSessionWithOptions(session.Options{
+	Profile:           "my_profile", // TODO 書き換え
+	SharedConfigState: session.SharedConfigEnable,
+},
+)))
+
+func main() {
+
+	params := &apigateway.TestInvokeMethodInput{
+		RestApiId:           aws.String("1234123412"), // TODO 書き換え
+		ResourceId:          aws.String("3gapai"),     // TODO 書き換え
+		HttpMethod:          aws.String(http.MethodGet),
+		PathWithQueryString: aws.String("/v1/health"),
+	}
+
+	req, resp := apigw.TestInvokeMethodRequest(params)
+	if err := req.Send(); err != nil {
+		log.Fatalf("test invoke: %v", err)
+	}
+	fmt.Printf("%d\n%v\n%v\n", *resp.Status, resp.Headers, *resp.Body)
+}
